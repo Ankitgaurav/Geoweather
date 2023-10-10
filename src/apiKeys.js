f7b141b1ffe8b210ade9bb328ad66d81960409c5
Ankitgaurav <ankitgaurav5139@gmail.com>
2023-10-11 03:58:13 +0530

    Weather app files

---
+++ src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "034fb7c047ba021f6ee74d6706f9c2d3",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
